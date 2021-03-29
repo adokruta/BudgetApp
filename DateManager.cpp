5499c392f7a202c7aedc48767f24083ad56c46fd
adokruta <adokruta@gmail.com>
2021-03-29 19:29:28 +0200

    Added class DateManager

---
+++ DateManager.cpp
@@ -0,0 +1,4 @@
+#include "DateManager.h"
+
+
+
---
+++ DateManager.h
@@ -0,0 +1,20 @@
+#ifndef DATAMANAGER_H
+#define DATAMANAGER_H
+
+
+#include "AuxiliaryMethods.h"
+
+
+using namespace std;
+
+class DataManager
+{
+
+
+public:
+
+
+
+};
+
+#endif
